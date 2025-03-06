c03feb7e6689f7fd7b8bee23ef9bc1dc46e92878
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-03-06 17:34:06 -0300

    respnsividade

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,10 +1,8 @@
 export const wordsList = {
-    //carro: ["Motor", "Rodas", "Bancos", "Faróis", "Portas", "Vidros", "Óleo", "Freios", "Transmissão", "Ignição", "Marcha", "Bateria", "Suspensão", "Escape", "Tanque"],
-    //fruta: ["Maçã", "Banana", "Morango", "Uva", "Abacaxi", "Melancia", "Laranja", "Pera", "Kiwi", "Manga", "Pêssego", "Cereja", "Limão", "Goiaba", "Melão"],
-    //corpo: ["Cabeça", "Olhos", "Nariz", "Boca", "Orelhas", "Braços", "Pernas", "Mãos", "Pés", "Dedos", "Joelhos", "Cotovelo", "Cabelo", "Pele", "Costas"],
-    computador: [ "Periféricos"],
-    //programação: ["Variável", "Função", "Loop", "Array", "Condição", "Algoritmo", "Classe", "Objeto", "Método", "Compilação", "Depuração", "Linguagem", "Interface", "Framework", "Código"],
-    //alimento: ["Arroz", "Feijão", "Carne", "Vegetais", "Frango", "Peixe", "Salada", "Massa", "Pão", "Frutas", "Queijo", "Ovos", "Sopa", "Bebida", "Doce"]
+    carro: ["Motor", "Rodas", "Bancos", "Faróis", "Portas", "Vidros", "Óleo", "Freios", "Transmissão", "Ignição", "Marcha", "Bateria", "Suspensão", "Escape", "Tanque"],
+    fruta: ["Maçã", "Banana", "Morango", "Uva", "Abacaxi", "Melancia", "Laranja", "Pera", "Kiwi", "Manga", "Pêssego", "Cereja", "Limão", "Goiaba", "Melão"],
+    corpo: ["Cabeça", "Olhos", "Nariz", "Boca", "Orelhas", "Braços", "Pernas", "Mãos", "Pés", "Dedos", "Joelhos", "Cotovelo", "Cabelo", "Pele", "Costas"],
+    computador: ["CPU", "Monitor", "Teclado", "Mouse", "Disco Rígido", "Memória RAM", "Gráficos", "Software", "Hardware", "Periféricos", "Rede", "USB", "Tela"],
+    programação: ["Variável", "Função", "Loop", "Array", "Condição", "Algoritmo", "Classe", "Objeto", "Método", "Compilação", "Depuração", "Linguagem", "Interface", "Framework", "Código"],
+    alimento: ["Arroz", "Feijão", "Carne", "Vegetais", "Frango", "Peixe", "Salada", "Massa", "Pão", "Frutas", "Queijo", "Ovos", "Sopa", "Bebida", "Doce"]
 };
-
-//computador: ["CPU", "Monitor", "Teclado", "Mouse", "Disco Rígido", "Memória RAM", "Gráficos", "Software", "Hardware", "Periféricos", "Rede", "USB", "Tela"],
